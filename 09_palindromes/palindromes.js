6e6b1b966965be40efd17d503353cdbeb3c4abe1
yuvrajshekhawat1989 <yuvraj114941@gmail.com>
2023-05-11 14:05:04 +0530

    used replace method of string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,15 @@
 const palindromes = function (str) {
-  //lets make a string wthout punctuations,fullstop in lowercase
-  new_str = "";
-  for (let i = 0; i < str.length; i++) {
-    if (str[i].toLowerCase() <= "z" && str[i].toLowerCase() >= "a")
-      new_str +=(str[i].toLowerCase());
-  }
-  reversed_new_str = new_str.split("").reverse().join("")
-  if (reversed_new_str === new_str) return true;
-  else return false;
+  // //lets make a string wthout punctuations,fullstop in lowercase
+  // new_str = "";
+  // for (let i = 0; i < str.length; i++) {
+  //   if (str[i].toLowerCase() <= "z" && str[i].toLowerCase() >= "a")
+  //     new_str +=(str[i].toLowerCase());
+  // }
+  // reversed_new_str = new_str.split("").reverse().join("")
+  // if (reversed_new_str === new_str) return true;
+  // else return false;
+  const processed_str = str.toLowerCase().replace(/[^a-z]/g, "");
+  return processed_str.split("").reverse().join("") == processed_str;
 };
 palindromes('Animal loots foliated detail of stool lamina.')
 // Do not edit below this line
