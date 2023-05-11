f22e6baa24dc7a882b2c60aa2e72eda179fa3bc7
yuvrajshekhawat1989 <yuvraj114941@gmail.com>
2023-05-11 13:35:30 +0530

    Used filter method of array to do 4th question

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,22 +1,6 @@
-function removeItem(arr,element)
-{
-    let i = 0;
-    while(i<arr.length)
-    {
-        if(arr[i]===element)
-        arr.splice(i,1);
-        i++;
-    }
-    return arr;
-}
-
-const removeFromArray = function () {
-  array = arguments[0];
-  for (let i = 1; i < arguments.length; i++) {
-    itemToRemove = arguments[i];
-    array = removeItem(array,itemToRemove)
-  }
-  return array;
+const removeFromArray = function (array, ...args) {
+  new_array = [];
+  return array.filter(element=>{return !(args.includes(element))})
 };
 
 // Do not edit below this line
